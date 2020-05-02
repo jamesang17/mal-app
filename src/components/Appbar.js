e34da92554bd2f7c0e196c6e5d37ec1e87019ccf
Vinyou Tamprateep <vtamprateep@gmail.com>
2020-05-02 11:55:47 -0400

    Aggregated imports in Appbar.js

--- src/components/Appbar.js
+++ src/components/Appbar.js
@@ -1,13 +1,9 @@
 import React from 'react';
 import theme from '../themes/Theme';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import Button from '@material-ui/core/Button';
-import IconButton from '@material-ui/core/IconButton';
+import { AppBar, Toolbar, Typography,
+        Button, IconButton, Menu,
+        MenuItem } from '@material-ui/core';
 import MenuIcon from '@material-ui/icons/Menu';
-import Menu from '@material-ui/core/Menu';
-import MenuItem from '@material-ui/core/MenuItem';
 import AccountCircle from '@material-ui/icons/AccountCircle';
 import { ThemeProvider } from '@material-ui/core/styles';
 import firebase from '../firebase';
