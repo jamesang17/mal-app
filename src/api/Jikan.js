65c10e4fecdf56a8f6fc9626a14d46e45dd691d7
jamesang <jamesang717@gmail.com>
2020-05-05 11:39:53 -0400

    export anime endpoint functions

--- src/api/Jikan.js
+++ src/api/Jikan.js
@@ -1,9 +1,11 @@
 import { searchAnime, searchManga, searchCharacter, SearchObj } from './functions/search';
 import { topAnime, topManga, AnimeTopTypes, MangaTopTypes } from './functions/top';
 import { getAnimeInGenre, getMangaInGenre, GenreIds, GenreObj } from './functions/genre';
+import { getAnimeInfo, getAnimeRecommendations, getAnimeReviews, getAnimeStats } from './functions/anime';
 
 export { 
   searchAnime, searchManga, searchCharacter,
   topAnime, topManga, AnimeTopTypes, MangaTopTypes, SearchObj,
-  getAnimeInGenre, getMangaInGenre, GenreIds, GenreObj 
+  getAnimeInGenre, getMangaInGenre, GenreIds, GenreObj,
+  getAnimeInfo, getAnimeRecommendations, getAnimeReviews, getAnimeStats
 };
