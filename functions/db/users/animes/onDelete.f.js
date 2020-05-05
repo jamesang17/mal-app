be808b47b96d1f926336987209a57c3fb70e1186
jamesang <jamesang717@gmail.com>
2020-05-05 13:10:21 -0400

    remove auth check in user onDelete

--- functions/db/users/animes/onDelete.f.js
+++ functions/db/users/animes/onDelete.f.js
@@ -3,14 +3,6 @@ const admin = require('firebase-admin');
 try { admin.initializeApp(functions.config().firebase); } catch (e) { null } // You do that because the admin SDK can only be initialized once.
 
 exports = module.exports = functions.https.onCall(async (data, context) => {
-  // check if user is authenticated
-  if (!context.auth) {
-    throw new functions.https.HttpsError(
-      'unauthenticated',
-      'You must be logged in to save anime.'
-    );
-  }
-
   // get user doc ref
   const user = admin.firestore().collection('users').doc(context.auth.uid);
 
