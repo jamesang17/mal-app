f8cdb47e8ccc3601ff881454fcec27928404e391
Vinyou Tamprateep <vtamprateep@gmail.com>
2020-05-07 09:59:45 -0400

    create dashboard component

--- src/components/CardCarousel.js
+++ src/components/CardCarousel.js
@@ -1,8 +1,9 @@
 import React from 'react';
-import { Card, CardHeader, CardMedia, CardContent } from '@material-ui/core';
+import { Card, CardActions, CardMedia, CardContent, IconButton, Typography } from '@material-ui/core';
 import Carousel from 'react-multi-carousel';
 import 'react-multi-carousel/lib/styles.css';
 import { topAnime } from '../api/Jikan';
+import FavoriteIcon from '@material-ui/icons/Favorite';
 
 
 class CardCarousel extends React.Component {
@@ -15,11 +16,13 @@ class CardCarousel extends React.Component {
 
     componentDidMount() {
         // TODO: replace hard coded value with input valus
+        
         topAnime(1).then( res => {
             this.setState ({
                 results: res
             })     
         })
+        
     }
     
     render () {
@@ -36,22 +39,26 @@ class CardCarousel extends React.Component {
             superLargeDesktop: {
             breakpoint: { max: 4000, min: 3000 },
             items: 5,
-            slidesToSlide: 5
+            slidesToSlide: 5,
+            maxWidth: "20vh"
             },
             desktop: {
             breakpoint: { max: 3000, min: 1024 },
             items: 4,
-            slidesToSlide: 4
+            slidesToSlide: 4,
+            maxWidth: "25vh"
             },
             tablet: {
             breakpoint: { max: 1024, min: 464 },
             items: 3,
-            slidesToSlide: 3
+            slidesToSlide: 3,
+            maxWidth: "33vh"
             },
             mobile: {
             breakpoint: { max: 464, min: 0 },
             items: 1,
-            slidesToSlide: 5
+            slidesToSlide: 1,
+            maxWidth: "95vh"
             }
         };
 
@@ -62,19 +69,22 @@ class CardCarousel extends React.Component {
                 showDots={true}
             >
                 {this.state.results.map( item => (
-                    <Card style={{maxWidth: "20vw" , padding:"2%", margin:"5%", height:"85%"}} raised={true} key={item["rank"]}>
-                        <CardHeader
-                            title={item["title"]}
-                            subheader={"Start Date: " + item["start_date"]}
-                        />
+                    <Card style={{ padding:"2%", margin:"5%", height:"85%" }} raised={true}>
                         <CardMedia 
                             square="true"
                             image={item["image_url"]}
                             style={cardMedia.media}
                         />
                         <CardContent>
+                            <Typography style={{ fontWeight: "bold" }}>{item["title"]}</Typography>
+                            <Typography>{"Start Date: " + item["start_date"]}</Typography>
                             Rank: {item["rank"]}
                         </CardContent>
+                        <CardActions>
+                            <IconButton>
+                                <FavoriteIcon />
+                            </IconButton>
+                        </CardActions>
                     </Card>
                 ))}
             </Carousel>
---
+++ src/components/Dashboard.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { searchAnime, searchManga, searchCharacter,
+    topAnime, topManga, AnimeTopTypes, MangaTopTypes, SearchObj,
+    getAnimeInGenre, getMangaInGenre, GenreIds, GenreObj,
+    getAnimeInfo, getAnimeRecommendations, getAnimeReviews, getAnimeStats,
+    Anime } from '../api/Jikan';
+
+
+const Dashboard = () => {
+
+    
+
+}
