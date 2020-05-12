e7e19e2107d975165d35cc8ecddba87da8a4f91c
jamesang <jamesang717@gmail.com>
2020-05-12 09:26:46 -0400

    use backdrop instead of transparent dialog

--- src/components/CustomBackdrop.js
+++ src/components/CustomBackdrop.js
@@ -1,12 +1,13 @@
 import React from 'react';
-import { Dialog, CircularProgress } from '@material-ui/core';
+import { Dialog, CircularProgress, Backdrop } from '@material-ui/core';
 
 const CustomBackdrop = (props) => {
 
   return(
-    <Dialog open={props.shouldOpen} style={{ zIndex: 9999999999, color: "#fff" }}
-      PaperProps={{ style: { background: "transparent", boxShadow: "none" } }} >
-      <CircularProgress style={{ color: "white" }} />
+    <Dialog open={props.shouldOpen} style={{ zIndex: 9999999999 }}>
+      <Backdrop open={true}>
+        <CircularProgress style={{ color: "white" }} />
+      </Backdrop>
     </Dialog>
   )
 }
